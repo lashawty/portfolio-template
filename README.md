e91ef96928015459d6dc2382b017b91be762f559
Sean Coding <hustle0985@gmail.com>
2023-02-28 13:26:56 +0800

    finish

--- README.md
+++ README.md
@@ -284,9 +284,169 @@ const tick = () =>
 }
 ```
 
-### Easing
+### 滑鼠 Easing
 
 ```
-cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 0.1
-cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 0.1
+cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 0.5
+cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 0.5
+```
+
+### onClick 動畫
+
+#### 新增隕石
+![](https://i.imgur.com/4a7NLPP.png)
+
+
+```
+//隕石
+const meteoriteTexture = textureLoader.load('textures/gradients/stone.jpeg')
+const meteoriteMaterial = new THREE.MeshPhongMaterial({ 
+    color: parameters.materialColor, 
+    alphaMap: meteoriteTexture//帶入紋理
+})
+
+let meteorites = []
+let animating = false
+const meteoAnimateTl = () => {
+    const meteoriteGeo = new THREE.DodecahedronGeometry(
+        gsap.utils.random(0, 3, .1), 
+        gsap.utils.random(0, 1, 1)
+    )
+    const meteorite = new THREE.Mesh(meteoriteGeo, meteoriteMaterial)
+
+    meteorite.scale.set(.1, .1, .1)
+    meteorite.position.set(0,0,gsap.utils.random(1, 5, 1))
+    scene.add(meteorite)
+    meteorites.push(meteorite)
+    
+    const tl = gsap.timeline()
+    tl.to(meteorite.position, {
+        x: gsap.utils.random(-2, 2, .1),
+        y: gsap.utils.random(-10, 1, .1),
+        z: gsap.utils.random(2, 4, .1),
+        duration: 5,
+        delay: .2,
+    })
+    .to(meteorite.rotation, {
+        x: "+=10",
+        y: "+=10",
+        z: "+=10",
+        duration: 5,
+    },"<")
+    
+}
+
+const metroriteClass = document.querySelector('.meteorite')
+metroriteClass.addEventListener('click', function () {
+    meteoAnimateTl()
+})
+```
+
+#### 流星雨
+![](https://i.imgur.com/jiQ9JnZ.png)
+
+
+```
+//流星雨
+let raining = false
+const newParticles = new THREE.Points(particlesGeometry, particlesMaterial)
+const particlesAnimate = () => {
+    scene.add(newParticles)
+    if (!raining) {
+        gsap.to(particles.position, {
+            x: `+=${gsap.utils.random(-15,15,1)}`,
+            y: "+=-10",
+            z: `+=${gsap.utils.random(-2,2,1)}`,
+            stagger: 1,
+            duration: gsap.utils.random(1,10,.5),
+            onStart: self => {raining = true},
+            onComplete: self => {
+                scene.remove(particles)
+                // scene.add(particles)
+                scene.add(particles)
+                particles.position.set(0,0,0)
+                raining = false
+            }
+        })
+    }
+}
+const shootingStar = document.querySelector('.shooting-stars')
+shootingStar.addEventListener('click', particlesAnimate)
+```
+
+#### 黑洞
+
+![](https://i.imgur.com/W4TdCKE.png)
+
+```
+//黑洞
+const holeGeometry = new THREE.TorusGeometry( 1, .8, 12, 48, )
+const holeMaterial = new THREE.MeshPhongMaterial( { 
+    color: 0x000000,
+    bumpMap: meteoriteTexture
+} )
+const hole = new THREE.Mesh( holeGeometry, holeMaterial)
+hole.scale.set(.1, .1, .1)
+hole.position.set(5, objectsDistance * -2, 1.5)
+scene.add( hole )
+const blackHole = document.querySelector('.black-hole')
+let sucking = false
+gsap.to(hole.rotation, {
+    repeat: -1,
+    z: 5,
+    ease: "bounce"
+ }
+)
+blackHole.addEventListener('click', function () {
+    if (sucking) return
+    sucking = true
+    
+    const tl = gsap.timeline()
+    let positionArr = [mesh1.position, mesh2.position, mesh3.position, newParticles.position, particles.position]
+    let scaleArr = [mesh1.scale, mesh2.scale, mesh3.scale, newParticles.scale, particles.scale]
+    
+    tl
+    .to(hole.position, {
+        x: 1.5,
+        duration: 2,
+    })
+    .to(hole.scale, {
+        x: .6,
+        y: .6,
+        z: .6,
+        duration: 2
+    }, "<")
+    .to(positionArr, {
+        x: 2,
+        y: objectsDistance * -2,
+        z: 0,
+    })
+    .to(scaleArr, {
+        x: 0,
+        y: 0,
+        z: 0,
+    }, "<")
+    .to(hole.scale,{
+        x: 0,
+        y: 0,
+        z: 0,
+        onComplete: self => {
+            sucking = false
+        }
+    })
+    meteorites.forEach(e => {
+        gsap.to(e.position, {
+            x: 2,
+            y: objectsDistance * -2,
+            z: 0,
+            delay: 2,
+        })
+        gsap.to(e.scale, {
+            x: 0,
+            y: 0,
+            z: 0,
+            delay: 2,
+        })
+    })
+})
 ```
--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import './style.css'
 import * as THREE from 'three'
 import * as dat from 'lil-gui'
 import gsap from 'gsap'
-
 // Debug
 const gui = new dat.GUI()
 
@@ -35,7 +34,6 @@ gradientTexture.magFilter = THREE.NearestFilter
 const material = new THREE.MeshToonMaterial({ 
     color: parameters.materialColor, 
     gradientMap: gradientTexture //帶入紋理
-    
 })
 
 // 帶入材質及形狀
@@ -53,6 +51,49 @@ const mesh3 = new THREE.Mesh(
 )
 
 scene.add(mesh1, mesh2, mesh3)
+
+//隕石
+const meteoriteTexture = textureLoader.load('textures/gradients/stone.jpeg')
+const meteoriteMaterial = new THREE.MeshPhongMaterial({ 
+    color: parameters.materialColor, 
+    alphaMap: meteoriteTexture//帶入紋理
+})
+
+let meteorites = []
+let animating = false
+const meteoAnimateTl = () => {
+    const meteoriteGeo = new THREE.DodecahedronGeometry(
+        gsap.utils.random(0, 3, .1), 
+        gsap.utils.random(0, 1, 1)
+    )
+    const meteorite = new THREE.Mesh(meteoriteGeo, meteoriteMaterial)
+
+    meteorite.scale.set(.1, .1, .1)
+    meteorite.position.set(0,0,gsap.utils.random(1, 5, 1))
+    scene.add(meteorite)
+    meteorites.push(meteorite)
+    
+    const tl = gsap.timeline()
+    tl.to(meteorite.position, {
+        x: gsap.utils.random(-2, 2, .1),
+        y: gsap.utils.random(-10, 1, .1),
+        z: gsap.utils.random(2, 4, .1),
+        duration: 5,
+        delay: .2,
+    })
+    .to(meteorite.rotation, {
+        x: "+=10",
+        y: "+=10",
+        z: "+=10",
+        duration: 5,
+    },"<")
+    
+}
+
+const metroriteClass = document.querySelector('.meteorite')
+metroriteClass.addEventListener('click', function () {
+    meteoAnimateTl()
+})
 
 //定義一個距離常數
 const objectsDistance = 4
@@ -115,7 +156,6 @@ window.addEventListener('scroll', () =>
 {
     scrollY = window.scrollY
     const newSection = Math.round(scrollY / sizes.height)
-    
     if(newSection != currentSection)
     {
         currentSection = newSection
@@ -163,13 +203,14 @@ const clock = new THREE.Clock()
 let previousTime = 0 //宣告時間變數以便建立時間差
 
 // 粒子背景
-const particlesCount = 200
+const particlesCount = 1000
 const positions = new Float32Array(particlesCount * 3)
 for(let i = 0; i < particlesCount; i++)
 {
     positions[i * 3 + 0] = (Math.random() - 0.5) * 10
     positions[i * 3 + 1] = objectsDistance * 0.5 - Math.random() * objectsDistance * sectionMeshes.length
     positions[i * 3 + 2] = (Math.random() - 0.5) * 10
+    
 }
 const particlesGeometry = new THREE.BufferGeometry()
 particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
@@ -182,6 +223,104 @@ const particlesMaterial = new THREE.PointsMaterial({
 // Points
 const particles = new THREE.Points(particlesGeometry, particlesMaterial)
 scene.add(particles)
+
+//流星雨
+let raining = false
+const newParticles = new THREE.Points(particlesGeometry, particlesMaterial)
+const particlesAnimate = () => {
+    scene.add(newParticles)
+    if (!raining) {
+        gsap.to(particles.position, {
+            x: `+=${gsap.utils.random(-15,15,1)}`,
+            y: "+=-10",
+            z: `+=${gsap.utils.random(-2,2,1)}`,
+            stagger: 1,
+            duration: gsap.utils.random(1,10,.5),
+            onStart: self => {raining = true},
+            onComplete: self => {
+                scene.remove(particles)
+                // scene.add(particles)
+                scene.add(particles)
+                particles.position.set(0,0,0)
+                raining = false
+            }
+        })
+    }
+}
+const shootingStar = document.querySelector('.shooting-stars')
+shootingStar.addEventListener('click', particlesAnimate)
+
+//黑洞
+const holeGeometry = new THREE.TorusGeometry( 1, .8, 12, 48, )
+const holeMaterial = new THREE.MeshPhongMaterial( { 
+    color: 0x000000,
+    bumpMap: meteoriteTexture
+} )
+const hole = new THREE.Mesh( holeGeometry, holeMaterial)
+hole.scale.set(.1, .1, .1)
+hole.position.set(5, objectsDistance * -2, 1.5)
+scene.add( hole )
+const blackHole = document.querySelector('.black-hole')
+let sucking = false
+gsap.to(hole.rotation, {
+    repeat: -1,
+    z: 5,
+    ease: "bounce"
+ }
+)
+blackHole.addEventListener('click', function () {
+    if (sucking) return
+    sucking = true
+    
+    const tl = gsap.timeline()
+    let positionArr = [mesh1.position, mesh2.position, mesh3.position, newParticles.position, particles.position]
+    let scaleArr = [mesh1.scale, mesh2.scale, mesh3.scale, newParticles.scale, particles.scale]
+    
+    tl
+    .to(hole.position, {
+        x: 1.5,
+        duration: 2,
+    })
+    .to(hole.scale, {
+        x: .6,
+        y: .6,
+        z: .6,
+        duration: 2
+    }, "<")
+    .to(positionArr, {
+        x: 2,
+        y: objectsDistance * -2,
+        z: 0,
+    })
+    .to(scaleArr, {
+        x: 0,
+        y: 0,
+        z: 0,
+    }, "<")
+    .to(hole.scale,{
+        x: 0,
+        y: 0,
+        z: 0,
+        onComplete: self => {
+            sucking = false
+        }
+    })
+    meteorites.forEach(e => {
+        gsap.to(e.position, {
+            x: 2,
+            y: objectsDistance * -2,
+            z: 0,
+            delay: 2,
+        })
+        gsap.to(e.scale, {
+            x: 0,
+            y: 0,
+            z: 0,
+            delay: 2,
+        })
+    })
+})
+
 
 const tick = () =>
 {
@@ -197,6 +336,7 @@ const tick = () =>
         // mesh.rotation.y = elapsedTime * 0.12
         mesh.rotation.x += deltaTime * 0.1
         mesh.rotation.y += deltaTime * 0.12
+        // hole.rotation.y += elapsedTime 
     }
 
     // 視角動畫
